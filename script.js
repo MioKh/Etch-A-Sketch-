d958875c07bba74fdc18fb237838291921dfbf30
Amro Khalfa <mioakiyamakh@gmail.com>
2023-02-22 00:27:15 +0300

    Added the options with a slider and a color input

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-let i = prompt("grid size");
-let gridSize = i * i;
-let widthPercentage = 100 / i;
-console.log(widthPercentage);
+// let i = prompt("grid size");
+// let gridSize = i * i;
+// let widthPercentage = 100 / i;
+// console.log(widthPercentage);
 
-const gridContainor = document.querySelector(".grid");
+// const gridContainor = document.querySelector(".grid");
 
-// Generate the grid divs and add them to the grid container
-for (let j = 0; j < gridSize; j++) {
-  const gridDiv = document.createElement("div");
-  gridDiv.className = "grid-div";
-  gridDiv.style.width = `${widthPercentage}%`;
-  gridDiv.style.height = `${widthPercentage}%`;
-  gridContainor.appendChild(gridDiv);
-}
+// // Generate the grid divs and add them to the grid container
+// for (let j = 0; j < gridSize; j++) {
+//   const gridDiv = document.createElement("div");
+//   gridDiv.className = "grid-div";
+//   gridDiv.style.width = `${widthPercentage}%`;
+//   gridDiv.style.height = `${widthPercentage}%`;
+//   gridContainor.appendChild(gridDiv);
+// }
+
+const rangeInput = document.querySelector('.slider');
+const rangeValue = document.querySelector('.slider-value');
+
+rangeValue.textContent = `${rangeInput.value} x ${rangeInput.value}`;
+
+rangeInput.addEventListener('input', () => {
+  rangeValue.textContent = `${rangeInput.value} x ${rangeInput.value}`;
+});
